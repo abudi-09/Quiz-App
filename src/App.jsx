e513b0a270fb66972cfe9db5e9a4ececa03e3f28
abudi-09 <abdsalih229@gmail.com>
2025-04-09 18:30:33 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Quiz } from "./component/Quiz/Quiz";
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Quiz />
+    </div>
+  );
 };
